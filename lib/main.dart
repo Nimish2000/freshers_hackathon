5b8aa8bb0d079a50c8e34b72dfa23ec81369acea
Nimish2000 <nimishbajaj88@gmail.com>
2022-05-12 19:21:32 +0530

    Navigation Changed with routes

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,6 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   static const appTitle = 'Freshers Admin';
-
   @override
   Widget build(BuildContext context) {
     return  MaterialApp(
@@ -28,68 +27,11 @@ class MyApp extends StatelessWidget {
       ),
       title: appTitle,
       home: WelcomeScreen(),
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id: (context)=> WelcomeScreen(),
+      },
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              'Freshers 2k??',
-              style: TextStyle(
-                fontSize: 30.0,
-              ),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            CircleAvatar(
-              radius: 80.0,
-              child: Image.asset('assets/images/party.jpg'),
-            ),
-            SizedBox(
-              height: 48.0,
-            ),
-            FlatButton(
-              color: Colors.blueGrey,
-              padding: EdgeInsets.all(8.0),
-              child: Text('Login'),
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => LoginScreen()));
-              },
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            FlatButton(
-              color: Colors.blueGrey,
-              padding: EdgeInsets.all(8.0),
-              child: Text('Register'),
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => RegistrationScreen()));
-              },
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
