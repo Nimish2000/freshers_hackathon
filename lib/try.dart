77ad39694f2e379bc0fc76d26d45707bc6c5fe3c
Nimish2000 <nimishbajaj88@gmail.com>
2022-05-12 18:49:48 +0530

    Added Try file

---
+++ lib/try.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class trial extends StatelessWidget {
+  const trial({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
