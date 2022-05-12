19f03e082710245db7c6a404a2e6f9487a6700a4
Nimish2000 <nimishbajaj88@gmail.com>
2022-05-12 20:59:28 +0530

    Added Firebase Signup

--- lib/screens/signup/sign_up.dart
+++ lib/screens/signup/sign_up.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:freshers_hackathon/screens/signup/components/body.dart';
 
 class SignUpScreen extends StatelessWidget {
+  static const String id = "sign_up_screen";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
