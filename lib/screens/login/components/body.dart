a67b6e278fcee3696d0c12957def5f81a7ab5c4c
Nimish2000 <nimishbajaj88@gmail.com>
2022-05-12 20:58:38 +0530

    Added firebase authentication to login

--- lib/screens/login/components/body.dart
+++ lib/screens/login/components/body.dart
@@ -1,16 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:freshers_hackathon/screens/login/components/background.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:freshers_hackathon/welcomeScreen.dart';
 import 'package:freshers_hackathon/widgets/rounded_button.dart';
 import 'package:freshers_hackathon/widgets/already_have_an_account.dart';
 import 'package:freshers_hackathon/widgets/rounded_input_field.dart';
-import 'package:freshers_hackathon/widgets/text_field_container.dart';
 import 'package:freshers_hackathon/widgets/rounded_password_field.dart';
+import 'package:freshers_hackathon/firebase/firebase_auth/auth.dart';
+import 'package:freshers_hackathon/screens/signup/sign_up.dart';
 
-class Body extends StatelessWidget {
+class Body extends StatefulWidget {
   const Body({
     Key? key,
   }) : super(key: key);
+
+  @override
+  State<Body> createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  bool isLoading = true;
+  Auth auth = Auth();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,25 +52,44 @@ class Body extends StatelessWidget {
             RoundedInputField(
               hintText: "Your Email",
               onChanged: (value) {},
+              controller: emailController,
             ),
             RoundedPasswordField(
               onChanged: (value) {},
+              controller: passwordController,
             ),
             RoundedButton(
               text: "LOGIN",
-              press: () {},
+              press: () async {
+                print(emailController.text.toString());
+                print(passwordController.text.toString());
+                showDialog(
+                  context: context,
+                  builder: (ctx) => Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                );
+                bool loggedIn = await auth.login(
+                    emailController.text.toString(),
+                    passwordController.text.toString());
+                Navigator.pop(context);
+                if (loggedIn) {
+                  Navigator.pushNamed(context, welcomeScreen.id);
+                  print("Logged In successfully");
+                } else {
+                  emailController.clear();
+                  passwordController.clear();
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                    content: Text('Invalid Details'),
+                    backgroundColor: Colors.black54,
+                  ));
+                }
+              },
             ),
             SizedBox(height: size.height * 0.03),
             AlreadyHaveAnAccountCheck(
               press: () {
-                // Navigator.push(
-                //   context,
-                //   MaterialPageRoute(
-                //     builder: (context) {
-                //       return SignUpScreen();
-                //     },
-                //   ),
-                // );
+                Navigator.pushNamed(context, SignUpScreen.id);
               },
             ),
           ],
